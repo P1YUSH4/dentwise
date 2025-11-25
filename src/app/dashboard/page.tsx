006045c56be0acafe64150717e5ee1b6f48104ef
P1YUSH4 <98311649+P1YUSH4@users.noreply.github.com>
2025-11-26 00:25:59 +0530

    dashboard page added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,18 @@
 import Navbar from "@/components/Navbar";
-
+import WelcomeSection from "@/components/dashboard/WelcomeSection";
+import ActivityOverview from "@/components/dashboard/ActivityOverview";
+import MainActions from "@/components/dashboard/MainActions";
 
 function DashboardPage() {
   return (
-     <div>
+     <>
       <Navbar />
-      DASHBOARD
-      </div>
+       <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+        <WelcomeSection />
+        <MainActions />
+        <ActivityOverview />
+       </div>
+      </>
   );
 }
 
